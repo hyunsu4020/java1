1f6f23a14a1d9d9da91d6a71669617e3eff2814b
LeeHyunSu <76948939+hyunsu4020@users.noreply.github.com>
2022-09-02 23:47:26 +0900

    Add files via upload

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+	
+		//Character VS String
+		System.out.println("Hello World"); // String: Character들이 모여있는 문장
+		System.out.println('H'); // Character: 한 글자
+		System.out.println("H"); // String
+
+		System.out.println("Hello "
+				+ "World");
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\""); // Hello "World"
+	}
+
+}
